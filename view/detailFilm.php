755d4cc80ed467a9b5d614870c3e7062cdecab1e
slyriyad <156778836+slyriyad@users.noreply.github.com>
2024-03-31 18:37:55 +0200

    front detailFilm

--- view/detailFilm.php
+++ view/detailFilm.php
@@ -1,36 +1,39 @@
 <?php ob_start(); ?>
 <?php $film = $requeteFilm->fetch() ?>
+<link rel="stylesheet" type="text/css" href="public/css/detailFilm.css" />
 
-<img src="<?= $film["affiche"]?>" alt="">
-<strong>Realisateur :</strong>
-<p><a href="index.php?action=detailRealisateur&id=<?= $film["Id_realisateur"] ?>"><?= $film["prenom"]." ".$film["nom"]?></a></p>
-<strong>Année de sortie :</strong>
-<p><?= $film["anneeSortie"]?></p>
-<strong>Durée :</strong>
-<p><?= $film["duree"]?></p>
-<strong>Note :</strong>
-<p><?= $film["note"]?></p>
+<p class="titre" ><?= $film["titre"] ?></p >
+<div class="affiche">
+    <img src="<?= $film["affiche"]?>" alt="">
+</div>
+
+<div class="text">
+    <div class="realisateur">
+        <p class="cat" >Realisateur </p>
+        <img src="<?= $film["photo"] ?>" alt="">
+        <p><a class="nom" href="index.php?action=detailRealisateur&id=<?= $film["Id_realisateur"] ?>"><?= $film["prenom"]." ".$film["nom"]?></a></p>
+    </div>
+    <p class="cat">Année de sortie :</p>
+    <p><?= $film["anneeSortie"]?></p>
+    <p class="cat">Durée :</p>
+    <p><?= $film["duree"]?></p>
+    <p class="cat">Note :</p>
+    <p><?= $film["note"]?></p>
+</div>
 
 
-<h3>Casting</h3>
+<p class="cat" >Casting</p>
+<div class="swipper">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                <?php foreach($requeteCasting->fetchall() as $cast) { ?>
+                        <div class="swiper-slide"><a href="index.php?action=detailActeur&id=<?= $cast["Id_acteur"] ?>"><img src="<?= $cast["photo"] ?>  " alt=""><div class="nom"><?= $cast["prenom"] ?> <?= $cast["nomAct"] ?>  </div><a  href="index.php?action=detailRole&id=<?= $cast["Id_role"] ?>"><p class="role"><?= $cast["roleNom"] ?><p></a></div>
+                    <?php } ?>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
 
-<table>
-    <thead>
-        <tr>
-            <th>ACTEUR</th>
-            <th>ROLE</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($requeteCasting->fetchall() as $cast) { ?>
-            <tr>
-
-                <td><a href="index.php?action=detailActeur&id=<?= $cast["Id_acteur"] ?>"><?= $cast["prenom"] ?> <?= $cast["nomAct"] ?> </a></td>
-                <td><a href="index.php?action=detailRole&id=<?= $cast["Id_role"] ?>"><?= $cast["roleNom"] ?></td>
-            </tr>
-        <?php } ?>
-    </tbody>   
-</table>
 
 <?php
 $titre = "details $film[titre]";
